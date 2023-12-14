c159c241fb05ecc3ec4362bfd7392f868431a5b6
EkaterinaKochneva <kochnevaes@gmail.com>
2023-12-14 22:02:01 +0300

    chore: build project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -274,4 +274,37 @@ window.addEventListener('DOMContentLoaded', function () {
   }
   var addFileList = document.querySelectorAll('.add-file');
   addFileInput(addFileList);
+  function sectionSlider(elements) {
+    if (elements.length !== 0) {
+      elements.forEach(function (n) {
+        var slider = new Swiper(n.querySelector('.swiper'), {
+          loop: true,
+          slidesPerView: 1,
+          breakpoints: {
+            370: {
+              slidesPerView: 2,
+              spaceBetween: 10
+            },
+            767: {
+              slidesPerView: 2,
+              spaceBetween: 20
+            },
+            992: {
+              slidesPerView: 3,
+              spaceBetween: 30
+            }
+          },
+          navigation: {
+            nextEl: n.querySelector('.section-slider__button-next'),
+            prevEl: n.querySelector('.section-slider__button-prev')
+          },
+          pagination: {
+            el: n.querySelector('.section-slider__pagination')
+          }
+        });
+      });
+    }
+  }
+  var slidersList = document.querySelectorAll('.section-slider');
+  sectionSlider(slidersList);
 });
