3bebaee9e2ff4537a71fad27d9ddb4ee3644e7c8
EkaterinaKochneva <kochnevaes@gmail.com>
2023-12-19 18:43:46 +0300

    fix: add availability check to mobile menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,11 @@ const mobileMenu = () => {
   const body = document.querySelector('body');
   const mainMenu = document.querySelector('.top-menu');
   const burger = document.querySelector('.burger');
-  const close = document.querySelector('.top-menu__close');  
+  const close = document.querySelector('.top-menu__close');
+  
+  if( !mainMenu || !burger || !close) {
+    return false;
+  }
 
   burger.addEventListener('click', () => {
     mainMenu.classList.add('is-active');
