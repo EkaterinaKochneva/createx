0bf56cb69fa1f6312671b2798aa6fef8dc722c67
EkaterinaKochneva <kochnevaes@gmail.com>
2024-02-25 15:09:16 +0300

    fix: change mobile menu and header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,18 @@
 const mobileMenu = () => {
 
+  const body = document.querySelector('body');
   const header = document.querySelector('header');
   const mainMenu = document.querySelector('.top-menu');
   const burger = document.querySelector('.burger');
-  const close = document.querySelector('.top-menu__close');
   
   if( !mainMenu || !burger || !close) {
     return false;
   }
 
   burger.addEventListener('click', () => {
-    mainMenu.classList.add('is-active');
-    header.classList.add('faded');
-    
-  });
-  close.addEventListener('click', () => {
-    mainMenu.classList.remove('is-active');
-    header.classList.remove('faded');
+    body.classList.toggle('is-hidden');
+    mainMenu.classList.toggle('is-active');
+    header.classList.toggle('is-open');
     
   });
   
